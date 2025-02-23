af3458ee038385af73a9b6a3deea0dcaf842792e
Nikhil k <83801330+nikhil18440@users.noreply.github.com>
2025-02-23 17:39:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let sec1 = document.getElementsByClassName('sec1')[0] ;
 let j = 0 ;
 let k = 1 ;
 for (let i = 0; i < 18; i++) {     let img = document.createElement('img') ;                         img.setAttribute('id',`sec1Img${i}`) ;
-if(k<=6){         img.setAttribute('src', `Untitled-${k}.png`)  ;       k++     }else{         k=1 ;        img.setAttribute('src', `Untitled-${k}.png`)     } ;     img.style.position = 'absolute'  ;   img.style.zIndex = -1;     img.style.left = `${j}px` ;    j = j+80     ;     if(i%2!==0){         img.style.top = 0        ; img.setAttribute('class',`sec1ImgOdd`);
+if(k<=6){         img.setAttribute('src', `Untitled-${k}.png`)  ; img.setAttribute('alt', "")  ;      k++     }else{         k=1 ;        img.setAttribute('src', `Untitled-${k}.png`); img.setAttribute('alt', "")  ;     } ;     img.style.position = 'absolute'  ;   img.style.zIndex = -1;     img.style.left = `${j}px` ;    j = j+80     ;     if(i%2!==0){         img.style.top = 0        ; img.setAttribute('class',`sec1ImgOdd`);
 sec1.appendChild(img);
 }else{
  img.style.bottom = 0  ;img.setAttribute('class',`sec1ImgEven`);
